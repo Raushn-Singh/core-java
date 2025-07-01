31fb86cfce95dd1b638661e1a29c70ab797d61fb
Raushan-Singh <raushan786267@gmail.com>
2025-07-01 19:13:23 +0530

    comments

---
+++ 01 java intro/comments.java
@@ -0,0 +1,14 @@
+public class comments{
+  public static void main(String args[]){
+    // Single line comment
+    System.out.print("Hello World");
+
+    /*
+     * multiple line comment
+     */
+       System.out.print("Hello World");
+       
+
+
+  }
+}
