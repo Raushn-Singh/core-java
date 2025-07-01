6533c4b2b9e431b0c8c5a138734a66563623d2e4
Raushan-Singh <raushan786267@gmail.com>
2025-07-01 19:44:17 +0530

    EscapeSequence

---
+++ 01 java intro/EscapeSequence.java
@@ -0,0 +1,32 @@
+public class EscapeSequence{
+  public static void main(String args[]){
+ // System.out.println("Hello world 1");
+  //System.out.print("Raushan");
+  //System.out.print("Singh");
+
+
+
+    //  System.out.print("\nHello\n");
+    //  System.out.print("Raushan\n");
+    //  System.out.print("Singh\n");
+   
+    // System.out.print("Hello\t");
+    // System.out.print("Raushan\t");
+    // System.out.print("Singh\t");
+
+    
+  //  System.out.printf("Hii i am %d printf \n",56);
+
+  // System.out.print("Hello 'Rausahn' Singh");
+  // System.out.print("Hello "Raushan" Singh"); // This will throw an error
+
+  // System.out.println("it is \"Raushan\" Singh");
+  // System.out.println("it is \"Raushan's\"Home");
+  // Sytem.out.print("\"); //error
+
+     System.out.print("\\");
+       System.out.println("Aman\b ");//after every \b space or any character  is required
+       System.out.println("Aman\b.");//after every \b space or any character is required
+         System.out.println("Aman\b \b ");//after every \b space or any character  is required
+  }
+}
